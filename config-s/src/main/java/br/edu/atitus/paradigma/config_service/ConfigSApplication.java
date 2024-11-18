2c9b1c3de4bfc5614f94d3c6f071fbc91a142213
Joao Vitor N Luz <joaovitorluz950@gmail.com>
2024-11-18 17:51:38 -0300

    adicionando a atividade

---
+++ config-s/src/main/java/br/edu/atitus/paradigma/config_service/ConfigSApplication.java
@@ -0,0 +1,13 @@
+package br.edu.atitus.paradigma.config_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigSApplication.class, args);
+	}
+
+}
